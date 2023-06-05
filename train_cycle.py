52f5c2e89c6051c990d4698e4c226275fafb167f
flalini <flan101544@gmail.com>
2023-06-05 10:21:23 +0900

    SP_SEP_COUNT for colab ram

--- train_cycle.py
+++ train_cycle.py
@@ -7,14 +7,21 @@ from dual_network import dual_network
 from self_play import self_play
 from train_network import train_network
 from evaluate_network import evaluate_network
+import gc
+
+SP_SEP_COUNT = 10
 
 # 듀얼 네트워크 생성
 dual_network()
 
-for i in range(10):
+for i in range(1):
     print('Train', i, '====================')
     # 셀프 플레이 파트
-    self_play()
+    # 코랩에서 self_play 실행시 ram 용량이 꽉 차는 문제가 생겨
+    # SP_SEP_COUNT 만큼 나눠서 (실행 -> history 저장) 을 반복
+    for j in range(SP_SEP_COUNT):
+        self_play()
+        gc.collect()
 
     # 파라미터 변경 파트
     train_network()
